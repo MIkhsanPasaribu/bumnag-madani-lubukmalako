6cdfe43b2217861f2ab4a1c87aaf4f481b9a1be9
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2026-02-02 00:10:43 +0700

    Add alert component: implement layout with customizable message, icons, and dismiss functionality

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,60 @@
+{{-- Alert Component --}}
+@props([
+    'type' => 'info', // success, warning, danger, info
+    'message' => '',
+    'dismissible' => true
+])
+
+@php
+    $classes = [
+        'success' => 'alert-success',
+        'warning' => 'alert-warning',
+        'danger' => 'alert-danger',
+        'info' => 'alert-info',
+    ];
+    
+    $icons = [
+        'success' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />',
+        'warning' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />',
+        'danger' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />',
+        'info' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />',
+    ];
+    
+    $alertClass = $classes[$type] ?? $classes['info'];
+    $icon = $icons[$type] ?? $icons['info'];
+@endphp
+
+<div x-data="{ show: true }" 
+     x-show="show" 
+     x-transition:enter="transition ease-out duration-300"
+     x-transition:enter-start="opacity-0 transform -translate-y-2"
+     x-transition:enter-end="opacity-100 transform translate-y-0"
+     x-transition:leave="transition ease-in duration-200"
+     x-transition:leave-start="opacity-100 transform translate-y-0"
+     x-transition:leave-end="opacity-0 transform -translate-y-2"
+     class="{{ $alertClass }} flex items-start gap-3" 
+     role="alert">
+    
+    {{-- Icon --}}
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        {!! $icon !!}
+    </svg>
+    
+    {{-- Message --}}
+    <div class="flex-1">
+        @if($message)
+            {{ $message }}
+        @else
+            {{ $slot }}
+        @endif
+    </div>
+    
+    {{-- Dismiss Button --}}
+    @if($dismissible)
+        <button @click="show = false" class="flex-shrink-0 hover:opacity-70 transition-opacity" aria-label="Tutup">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+    @endif
+</div>
