6f69843f5b519470cb8a0605a5691154613e7b2d
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2026-02-06 23:36:08 +0700

    Fix date display to prioritize event date over publication date in news items

--- resources/views/public/berita/kategori.blade.php
+++ resources/views/public/berita/kategori.blade.php
@@ -89,7 +89,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                             </svg>
-                            {{ $item->tanggal_publikasi?->format('d M Y') }}
+                            {{ ($item->tanggal_kegiatan ?? $item->tanggal_publikasi)?->format('d M Y') }}
                         </span>
                         <span class="flex items-center gap-1">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
