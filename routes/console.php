fe05f34cd96bee3d942c7f6304df7b2581d5acc5
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2026-02-16 12:05:34 +0700

    style: Add daily command to auto-prune error logs older than 30 days

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Auto-prune error logs yang lebih dari 30 hari
+Schedule::command('model:prune', ['--model' => \App\Models\ErrorLog::class])->daily();
